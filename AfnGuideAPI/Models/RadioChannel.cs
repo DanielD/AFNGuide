45d304cf8080ed435d330181b77302a59d857954
Daniel Dhillon <daniel@dhillon.com>
2023-09-09 05:58:08 -0600

    Radio

---
+++ AfnGuideAPI/Models/RadioChannel.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AfnGuideAPI.Models
+{
+    [Table("RadioChannels")]
+    public class RadioChannel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? TagLine { get; set; }
+        public int? Duration { get; set; }
+        public byte[]? LogoData { get; set; }
+        public byte[]? LogoSlideData { get; set; }
+        public byte[]? LogoThumbnailData { get; set; }
+    }
+
+    public class JsonRadioChannel
+    {
+        [JsonProperty("a")]
+        public int Id { get; set; }
+        [JsonProperty("b")]
+        public string? Name { get; set; }
+        [JsonProperty("c")]
+        public string? Description { get; set; }
+        [JsonProperty("d")]
+        public string? TagLine { get; set; }
+        [JsonProperty("e")]
+        public string? Logo { get; set; }
+        [JsonProperty("f")]
+        public string? LogoSlide { get; set; }
+        [JsonProperty("g")]
+        public string? LogoThumbnail { get; set; }
+        [JsonProperty("h")]
+        public int? Duration { get; set; }
+    }
+}
---
+++ AfnGuideAPI/Models/RadioStation.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AfnGuideAPI.Models
+{
+    [Table("RadioStations")]
+    public class RadioStation
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Schedules { get; set; }
+        public byte[]? LogoData { get; set; }
+    }
+
+    public class JsonRadioStation
+    {
+        [JsonProperty("a")]
+        public int Id { get; set; }
+        [JsonProperty("b")]
+        public string? Name { get; set; }
+        [JsonProperty("c")]
+        public string? Description { get; set; }
+        [JsonProperty("d")]
+        public string? Logo { get; set; }
+        [JsonProperty("f")]
+        public List<JsonRadioStationSchedule>? Schedules { get; set; }
+    }
+
+    public class JsonRadioStationSchedule
+    {
+        [JsonProperty("a")]
+        public string? Day { get; set; }
+        [JsonProperty("b")]
+        public string? Schedule { get; set; }
+    }
+}
